e4898bbbb1b562828d005d020b596f140e085c9f
ABujalance <abujalance@deloittedigital.es>
2022-06-26 18:29:55 +0200

    add logout button

--- src/components/user-content/user-content.tsx
+++ src/components/user-content/user-content.tsx
@@ -1,5 +1,6 @@
 import { FC, useContext } from "react";
 import { userContext } from "../utils/user-context";
+import { Logout } from "./logout/logout";
 
 export const UserContent: FC = () => {
   const user = useContext(userContext);
@@ -7,6 +8,7 @@ export const UserContent: FC = () => {
   return (
     <div className="d-flex flex-column align-items-center">
       <p>Welcome, {user?.displayName}</p>
+      <Logout />
     </div>
   );
 };
